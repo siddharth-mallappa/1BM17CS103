3ec89c00a3b7f466e177494bd1b085449e00ee59
siddharth-mallappa <39707625+siddharth-mallappa@users.noreply.github.com>
2019-09-05 12:51:59 +0530

    Update 3B.py

--- 3B.py
+++ 3B.py
@@ -1,20 +1,29 @@
-from random import choice
-charsets=['abcdefghijklmnopqrstuvwxyz','ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-          '1234567890','!@#$%^$^&*()_+']
-def pswd(length=16):
-    pwd=[]
-    charset=choice(charsets)
-    while len(pwd)<length:
-        pwd.append(choice(charset))
-        charset=choice(list(set(charsets)-set([charset])))
-    return"".join(pwd)
+import random
+smallchar = 'abcdefghijklmnopqrstuvwxyz'
+bigchar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+splchar = '!@#$&%*^()'
+number = '1234567890'
+ctr = [0,0,0,0]
+def choose(stn):
+    return stn[random.randint(0, len(stn)-1)]
 
-print(pswd())
-while True:
-    c=1
-    c=int(input("want a new password? enter 1 for yes and 0 for no"))
-    if c==1:
-        print(pswd())
-    c=int(input("want a new password? enter 1 for yes and 0 for no"))
-    if c==0:
-        break
+length = random.randint(8, 15)
+pwd = ''
+for x in range(0, length):
+    while True:
+        avg = sum(ctr)/4
+        choice = random.randint(0,3)
+        if ctr[choice] <= avg:
+            break
+    if(choice == 0):
+        ch = choose(smallchar)
+    elif(choice == 1):
+        ch= choose(bigchar)
+    elif(choice == 2):
+        ch = choose(splchar)
+    else:
+        ch = choose(number)
+    ctr[choice] += 1
+    pwd += ch
+
+print(pwd)
